db1687b57362725a812ecd585f4262f7df065711
cibi-sudo <cibiccf437@gmail.com>
2025-12-09 15:54:45 +0530

    test

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    semi: true,
-    singleQuote: false,
-    printWidth: 100,
-    tabWidth: 2,
-    trailingComma: "all",
-    bracketSpacing: true,
-    arrowParens: "always"
-};
